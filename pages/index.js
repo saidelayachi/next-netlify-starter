07eff743aa8bc2257b428809b3499b0a23e57529
saidelayachi <61349271+saidelayachi@users.noreply.github.com>
2022-12-15 21:00:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,17 @@ export default function Home() {
 
       <main>
         <Header title="Welcome Babe" />
-        <p className="description">
+        <p className="description"></p>
 
-    
-          <div>
-          <form action="/action_page.php">
+  <form action="/action_page.php">
   <label for="fname">First name:</label>
   <input type="text" id="fname" name="fname"><br><br>
   <label for="lname">Last name:</label>
   <input type="text" id="lname" name="lname"><br><br>
   <input type="submit" value="Submit">
 </form>
-          </div>
-        </p>
+
+        
       </main>
 
       <Footer />
